2969fd680b1be4b666cf2e7e68cf2e66b2ebdad7
Maxibenz <145683261+Maxibenz@users.noreply.github.com>
2023-09-21 19:38:13 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parent new healthy Diner')
-streamlit.Header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Idly')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Omega 3 & Blackberry Oatmeal')
