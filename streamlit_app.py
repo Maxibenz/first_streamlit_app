fc1ce1429bdc5ded61c09fddadfb93505a003512
Maxibenz <145683261+Maxibenz@users.noreply.github.com>
2023-09-21 19:36:07 +0530

    Update streamlit_app.py
    Added Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parent new healthy Diner')
+streamlit.Header('Breakfast Menu')
+streamlit.text('Idly')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Omega 3 & Blackberry Oatmeal')
