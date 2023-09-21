2d2de0c4ce776e6462090b4011341568a75914eb
Maxibenz <145683261+Maxibenz@users.noreply.github.com>
2023-09-21 19:42:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit
-streamlit.title('  🐔  My parent new healthy Diner')
+streamlit.title('    My parent new healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🍞Idly')
 streamlit.text('🥣Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥗Omega 3 & Blackberry Oatmeal')
-streamlit.text('🥑 Avacado Toast')
+streamlit.Text('🥑 Avacado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
