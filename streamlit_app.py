77d15463e12a1cad8fa9adf624893925a22e4bf9
Maxibenz <145683261+Maxibenz@users.noreply.github.com>
2023-09-25 17:19:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
+
+
+      def get_fruity_vice_data(this_fruit_choice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+      #streamlit.text(fruityvice_response.json())
+      # write your own comment -what does the next line do? 
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+      return fruityvice_normalized
 streamlit.header('Fruityvice Fruit Advice!')
 
 
@@ -32,13 +42,11 @@ try:
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information")
   else: 
-      streamlit.write('The user entered ', fruit_choice)
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      #streamlit.text(fruityvice_response.json())
-      # write your own comment -what does the next line do? 
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      back_from_function = get_fruityvice_data(fruit_choice)
+      #streamlit.write('The user entered ', fruit_choice)
+      streamlit.dataframe(back_from_function)
       # write your own comment - what does this do?
-      streamlit.dataframe(fruityvice_normalized)
+      #streamlit.dataframe(fruityvice_normalized)
 
 except URLERROR as e:
   streamlit.error()
